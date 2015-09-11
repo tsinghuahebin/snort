3652f2f87d0d33c8e731826d227fd5449b15416d
hebin <491309649@qq.com>
2015-09-11 12:24:40 +0800

    modify the variable 'base_url' in variable.sh

--- variable.sh
+++ variable.sh
@@ -4,7 +4,8 @@
 # Tranfic from from_iface to to_iface
 from_iface='eth0'
 to_iface='eth1'
-base_url=$(cd "$(dirname "$0")"; pwd)
+#base_url=$(cd "$(dirname "$0")"; pwd)
+base_url=`dirname "$0"`
 log_url='/var/log/snort_install_log'
 snort_install_dir='/usr/local/etc/snort'
 home_net='192.168.23.0\/24'
